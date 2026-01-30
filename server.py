ada8077945b31f6a2a04327741a72dbe3df6cb3b
nabnabilaa <nabilamelsyana5@gmail.com>
2026-01-30 12:57:05 +0700

    update9

--- server.py
+++ server.py
@@ -58,9 +58,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://frontend-car-wash.vercel.app",
-        "http://localhost:3000",
-        "http://localhost:5173",
+        "*"
     ],
     allow_credentials=True,
     allow_methods=["*"],
